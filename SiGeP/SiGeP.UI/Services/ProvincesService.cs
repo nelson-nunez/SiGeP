a56d49f6cd0957abb9b9e512660152f28a7716ef
Matias Suarez <matiasgsuarez98@gmail.com>
2024-08-26 21:02:20 -0300

    Province list component and Patient List component

---
+++ SiGeP/SiGeP.UI/Services/ProvincesService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SiGeP.Model.DTO;
+
+namespace SiGeP.UI.Services
+{
+    //public class ProvincesService
+    //{
+    //    public async Task<List<ProvinceDTO>> GetProvincesAsync()
+    //    {
+           
+    //        var result = await baseApiClient.GetAsync<List<GenderDTO>>($"Gender");
+    //        return result;
+    //    }
+    //}
+}
